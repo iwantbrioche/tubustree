aaa7bb1e5a7e657fb41c18ca475b95807c5403ca
iwantbread <popeoompf@gmail.com>
2024-05-12 16:51:48 -0700

    Add project files.

---
+++ src/ModTemplate.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using System;
+using System.Security.Permissions;
+using System.Security;
+
+#pragma warning disable CS0618
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace ModTemplate
+{
+    [BepInPlugin(MOD_ID, "modname", "1.0.0")]
+    public class ModTemplate : BaseUnityPlugin
+    {
+        public const string MOD_ID = "name.id";
+        private void OnEnable()
+        {
+            On.RainWorld.OnModsInit += RainWorldOnOnModsInit;
+        }
+
+        private bool IsInit;
+
+        private void RainWorldOnOnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+        {
+            orig(self);
+            try
+            {
+                if (IsInit) return;
+
+                IsInit = true;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
+                throw;
+            }
+        }
+    }
+}
