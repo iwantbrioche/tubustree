91e770b013d9e0e2b1504eb6291108322abdfd30
iwantbread <popeoompf@gmail.com>
2024-05-12 17:03:04 -0700

    Add project files.

--- src/ModTemplate.cs
+++ src/ModTemplate.cs
@@ -1,7 +1,22 @@
-﻿using BepInEx;
-using System;
-using System.Security.Permissions;
+﻿global using System;
+global using System.Linq;
+global using System.Text.RegularExpressions;
+global using System.Collections.Generic;
+global using System.Reflection;
+global using BepInEx;
+global using MoreSlugcats;
+global using Mono.Cecil.Cil;
+global using MonoMod.Cil;
+global using UnityEngine;
+global using RWCustom;
+global using Debug = UnityEngine.Debug;
+global using Random = UnityEngine.Random;
+global using Vector2 = UnityEngine.Vector2;
+global using Color = UnityEngine.Color;
+global using Custom = RWCustom.Custom;
 using System.Security;
+using System.Security.Permissions;
+using BepInEx.Logging;
 
 #pragma warning disable CS0618
 
@@ -14,9 +29,13 @@ namespace ModTemplate
     public class ModTemplate : BaseUnityPlugin
     {
         public const string MOD_ID = "name.id";
+        public static new ManualLogSource Logger { get; private set; }
+        public static RemixTemplate remix;
         private void OnEnable()
         {
             On.RainWorld.OnModsInit += RainWorldOnOnModsInit;
+            remix = new RemixTemplate();
+            Logger = base.Logger;
         }
 
         private bool IsInit;
@@ -28,10 +47,13 @@ namespace ModTemplate
             {
                 if (IsInit) return;
 
+                MachineConnector.SetRegisteredOI(MOD_ID, remix);
+
                 IsInit = true;
             }
             catch (Exception ex)
             {
+                Logger.LogError("Mod failed to load!");
                 Logger.LogError(ex);
                 throw;
             }
---
+++ src/RemixTemplate.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ModTemplate
+{
+    public class RemixTemplate : OptionInterface
+    {
+        public RemixTemplate() 
+        {
+
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
